b3529ede32dee60fee3be5203d9ad54e64bd1c8f
Keesha4rmBrum <kjmurray@gmx.com>
2022-11-30 21:44:47 +0000

    Buttons created

---
+++ script.js
@@ -0,0 +1,6 @@
+var startButton = document.getElementById("startButton")
+var startPage = document.getElementById("startPage")
+startButton.addEventListener("click",function(){
+console.log("working")
+startPage.setAttribute("class","hide")
+})
